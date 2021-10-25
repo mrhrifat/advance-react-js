c63eaee6584ebfe51c8c64c709ab47c430eb4fa9
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-26 00:59:32 +0600

    Add import section for all js or jsx

---
+++ src/Import.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Import extends Component {
+    render() {
+        return (
+            <div>
+                <h6>Advance React JS</h6>
+            </div>
+        )
+    }
+}
+export default Import
