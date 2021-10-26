d3cd179ff4f20faa57452f8db80b952ccfcda94c
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-27 00:19:44 +0600

    Root files for import all JSX

--- src/Import.jsx
+++ src/Import.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import FragComp from "./Fragment/FragComp";
+import News from "./News/News";
+import Index from "./RegPureComp/Index";
+import Smart from "./SmartComponent/Smart";
 
 class Import extends Component {
     render() {
         return (
-            <div>
-                <h6>Advance React JS</h6>
+            <div className='container'>
+                <h3 className='text-center'>Advance React JS</h3>
+
+                {/*
+                <Smart /> 
+                <Index /> 
+                <FragComp /> 
+                
+                */}
+
+                <News/>
             </div>
         )
     }
