d65ee45084cd86543c72f7e160561bd856e7b655
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-28 00:52:44 +0600

    Update with REST API

--- src/Theory.js
+++ src/Theory.js
@@ -32,6 +32,55 @@
         04) <> </>
 
 
+3.1 Understand REST API
+    01) There are 3 ways to work with Data
+        01) REST API
+            01) REST - Representational State Transfer
+            02) API - Application Programming Interface
+        
+        02) There are 2 ways to work with REST API
+            01) Own server and API
+            02) Third Party API's
 
+    02) GhaphQL
+    03) Server Rendered
+
+3.2 Difference between API and URL
+    01) In API we get or have which much data we need not others data
+
+    02) In URL we get all data
+    
+    03) Software for work with REST Client
+        01) PostMan
+        02) Insomnia 
+            01) Website - insomnia.rest
+
+
+3.3 HTTPS Method
+    01) CRUD 
+        01) C - Create - POST
+        02) R - Read - GET
+        03) U - Update - PUT/PATCH
+        04) D - Delete - DELETE
+    
+    02) We can work with API data in 2 ways
+        01) XML HTTPS Resquest
+        02) Fetch
+            01) We can Fecth data esaily with a popular library 
+                01) AXIOS
+
+
+3.4 AXIOS 
+    01) GET
+        axios.get(url).then()
+
+    02) POST
+        axios.get(url,body).then()
+
+    03) PUT/PATCH
+        axios.put/patch(url,body).then()
+
+    04) DELETE
+        axios.delete(url).then()
 
 */
