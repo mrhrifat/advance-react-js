2d36691b3b59f3bc4ed96cf4d7c76317a1cd6888
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-28 01:02:50 +0600

    Upload news results components

---
+++ src/News/Components/Result.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class Result extends Component {
+    render() {
+        return (
+            <div className='d-flex justify-content-between'>
+                <p
+                    className='text-black-50'
+                >
+                    About {1} result found
+                </p>
+                <br />
+                <p
+                    className='text-black-50'
+                >
+                    {1} of {100}
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Result
