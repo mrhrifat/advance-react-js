fb47a80194a8c5e4a6697d33d662702cf0bc7c01
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-28 01:01:21 +0600

    Update News with Fake News and Others

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
 import Header from './Components/Header'
+import Loading from './Components/Loading'
+import NewsList from './Components/NewsList'
+import Paggination from './Components/Paggination'
+import Result from './Components/Result'
+import { newsTags } from './Utilites'
+
+const fakeNews = [
+    {
+        title: 'Title',
+        content: 'Content',
+        url: 'https://facebook.com',
+        urlToImage: 'https://facebook.com',
+        publishedAt: 'published at',
+        source: {
+            name: 'Facebook'
+        }
+    },
+    {
+        title: 'Title',
+        content: 'Content',
+        url: 'https://youtube.com',
+        urlToImage: 'https://youtube.com',
+        publishedAt: 'published at',
+        source: {
+            name: 'Youtube'
+        }
+    },
+]
 
 class News extends Component {
     render() {
         return (
-            <div className="w-50 text-center border m-auto p-2 my-5">
-                <Header />
+            <div className="w-100 border m-auto p-2 my-5">
+                <Header tags={newsTags.technology} />
+                <Result />
+                <NewsList news={fakeNews} />
+                <Paggination />
+                <Loading />
             </div>
         )
     }
