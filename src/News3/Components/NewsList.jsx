86f491e1d2a954435e17af52c7643c4c81c76644
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-03 21:52:45 +0600

    Upload NewsList Components

---
+++ src/News3/Components/NewsList.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const dateString = dateTimeString => {
+    return new Date(dateTimeString).toDateString()
+}
+
+const NewsCard = ({ item }) => {
+    return (
+        <div className='card mb-4'>
+            {item.urlToImage && (
+                <img
+                    className='card-img-top'
+                    src={item.urlToImage}
+                    alt={item.title}
+
+                />
+            )}
+            <div className="card-body">
+                <a
+                    href={item.url}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    style={{
+                        color: 'black',
+                        textDecoration: 'none',
+                    }}
+                >
+                    <h5>{item.title}</h5>
+                </a>
+
+                <a
+                    href={item.url}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    style={{
+                        color: 'black',
+                        textDecoration: 'none',
+                    }}
+                >
+                    <p>{item.content}</p>
+                </a>
+            </div>
+
+            <div className="card-footer d-flex align-item-center justify-content-between">
+                <small>
+                    Published at {dateString(item.publishedAt)}
+                </small>
+
+                <div
+                    style={{
+                        display: 'inline-block',
+                        borderRadius: '5px',
+                        background: '#198754',
+                        padding: '6px',
+                        borderRadius: '6px',
+                        color: '#fff',
+                    }}
+                >
+                    <small>{item.source.name}</small>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+
+
+const NewsList = ({ news }) => {
+    return (
+        <div>
+            {/* {console.log(news)} */}
+            {news && news.length === 0 && <h6>'There Is No News Today'</h6>}
+            {news && news.map(item =>
+                <NewsCard
+                    key={item.title}
+                    item={item}
+                />
+            )}
+        </div>
+    )
+}
+
+export default NewsList
