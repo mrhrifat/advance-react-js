d139990b8d1f0b400b25c330c80a990ad1124ded
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-03 21:52:14 +0600

    Upload Loading Components

---
+++ src/News3/Components/Loading.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Loading extends Component {
+    render() {
+        return (
+            <div className='my-4 d-flex align-item-center justify-content-between'>
+                <p>
+                    <strong>Loading...</strong>
+                </p>
+                <div
+                    className='spinner-border text-success' role='status' aria-hidden='true'
+                >
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Loading
